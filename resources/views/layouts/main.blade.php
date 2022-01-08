a03e84810a8a5b57964a40428ef003cc7e42a411
Douglas Duarte <douglasduarte.eng@gmail.com>
2022-01-08 13:55:47 -0300

    Flash message when submit form

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -34,7 +34,16 @@
                 </div>
             </nav>
         </header>
-        @yield('content')
+            <div class="container-fluid">
+                <div class="row">
+                    @if(session('msg'))
+                        <p class="msg">
+                            {{session('msg')}}
+                        </p>
+                    @endif
+                    @yield('content')
+                </div>
+            </div>
         <footer>
             <p>HDC Events &copy; 2022</p>
         </footer>
